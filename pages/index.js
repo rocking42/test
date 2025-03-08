d28f372432d210d666f61fbde39a2a30f303f38d
Ned Pike <ned.pike@kasada.io>
2025-03-08 18:32:15 +1100

    test commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my website</h1>
+      <p>This is the Next.js version of the site.</p>
+    </div>
+  );
+}
